26de8f9b75621029daacbb8334e0b4c1ae729e42
derickben <benedictuyioghosa@gmail.com>
2022-07-06 06:17:01 -0700

    m

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,53 +1,16 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
+ * print_name - a function that prints a name.
+ * @name: pointer to name to print
+ * @f: function to print name
+ * Return: Nothing
  */
-void print_name_as_is(char *name)
+void print_name(char *name, void (*f)(char *name))
 {
-  printf("Hello, my name is %s\n", name);
-}
-
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-  unsigned int i;
-
-  printf("Hello, my uppercase name is ");
-  i = 0;
-  while (name[i])
-    {
-      if (name[i] >= 'a' && name[i] <= 'z')
-        {
-	  putchar(name[i] + 'A' - 'a');
-        }
-      else
-        {
-	  putchar(name[i]);
-        }
-      i++;
-    }
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  print_name("Danton", print_name_as_is);
-  print_name("Rick Harris", print_name_uppercase);
-  printf("\n");
-  return (0);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
 
